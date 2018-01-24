771be8cb42507d3146bba2365e4d8b134bf5d3bd
prosild <35754074+prosild@users.noreply.github.com>
2018-01-24 04:06:14 +0000

    re

--- MUSICON/src/com/musicon/controller/BoardServlet.java
+++ MUSICON/src/com/musicon/controller/BoardServlet.java
@@ -1,8 +1,6 @@
 package com.musicon.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +12,7 @@ import com.musicon.controller.action.Action;
 /**
  * Servlet implementation class BoardServlet
  */
-@WebServlet("/board.do")
+@WebServlet("/BoardServlet")
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -31,21 +29,14 @@ public class BoardServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String command = request.getParameter("command");
+		System.out.println("BoardServlet에서 요청을 받음을 확인 : " + command);
 		
-		if(command != null) {
-			ActionFactory af = ActionFactory.getInstance();
-			Action action = af.getAction(command);
-			
-			if(action != null) {
-				action.execute(request, response);
-			}
-		}else {
-			String url = "board/board_list.jsp";
-			
-			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-			dispatcher.forward(request, response);
+		ActionFactory af = ActionFactory.getInstance();
+		Action action = af.getAction(command);
+		
+		if(action != null){
+			action.execute(request, response);
 		}
-
 	}
 
 	/**
